6d6ef3ade9f53309a731b899e54b955d82f0b3b6
dushyantsingh01234 <k363dushyant@gmail.com>
2026-09-16 22:50:00 +0530

    network module: vpc, subnets, nat per az

---
+++ infra/modules/network/main.tf
@@ -0,0 +1,95 @@
+data "aws_region" "current" {}
+
+locals {
+  az_names = [for a in var.azs : "${data.aws_region.current.name}${a}"]
+}
+
+resource "aws_vpc" "this" {
+  cidr_block           = var.cidr_block
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = merge(var.tags, { Name = "${var.name}-vpc" })
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.this.id
+  tags   = merge(var.tags, { Name = "${var.name}-igw" })
+}
+
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnets)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnets[count.index]
+  availability_zone       = local.az_names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-public-${var.azs[count.index]}"
+    Tier = "public"
+  })
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnets[count.index]
+  availability_zone = local.az_names[count.index]
+
+  tags = merge(var.tags, {
+    Name = "${var.name}-private-${var.azs[count.index]}"
+    Tier = "private"
+  })
+}
+
+# one NAT per az. cheaper prod option is a single NAT with cross-az routing,
+# but that breaks isolation if the az with the NAT goes down. keeping per-az.
+resource "aws_eip" "nat" {
+  count      = length(var.public_subnets)
+  domain     = "vpc"
+  depends_on = [aws_internet_gateway.this]
+  tags       = merge(var.tags, { Name = "${var.name}-nat-${var.azs[count.index]}" })
+}
+
+resource "aws_nat_gateway" "this" {
+  count         = length(var.public_subnets)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = merge(var.tags, { Name = "${var.name}-nat-${var.azs[count.index]}" })
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-public-rt" })
+}
+
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  count  = length(var.private_subnets)
+  vpc_id = aws_vpc.this.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
+
+  tags = merge(var.tags, { Name = "${var.name}-private-rt-${var.azs[count.index]}" })
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
+}
---
+++ infra/modules/network/variables.tf
@@ -0,0 +1,29 @@
+variable "name" {
+  type        = string
+  description = "Name prefix for resources"
+}
+
+variable "cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["a", "b"]
+}
+
+variable "public_subnets" {
+  type    = list(string)
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnets" {
+  type    = list(string)
+  default = ["10.0.11.0/24", "10.0.12.0/24"]
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
